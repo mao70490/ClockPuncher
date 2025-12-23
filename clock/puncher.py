19cfd658b9106998a7a73f1bed5599160b38c6c0
Mao <ccm841002@gmail.com>
2025-12-23 16:18:52 +0800

    update_2

--- clock/puncher.py
+++ clock/puncher.py
@@ -32,9 +32,6 @@ class Puncher:
         try:
             captcha_text = self._get_captcha_text(driver)
 
-            # 故意測試錯誤驗證碼
-            # captcha_text = "12333"
-
             driver.find_element(By.ID, "txtAccount").send_keys(self.config["username"])
             driver.find_element(By.ID, "txtPassword").send_keys(self.config["password"])
             driver.find_element(By.ID, "txtVerification").send_keys(captcha_text)
@@ -61,12 +58,11 @@ class Puncher:
                         logger.warning(msg)
                         return
                 else:
-                    # lblMessage 空白也視為失敗
-                    msg = f"{action_name}成功 ✅ \n但lblMessage 空白，沒有提示或是測試中，請檢查後台打卡紀錄"
+                    msg = f"{action_name}狀態不確定 \nlblMessage 空白，請檢查後台打卡紀錄"
                     send_line_message(msg)
                     logger.warning(msg)
             except Exception:
-                msg = f"{action_name}失敗 ❌ \n未找到 lblMessage 元素，可能登入流程沒有該訊息"
+                msg = f"{action_name}失敗 ❌ \n未找到 lblMessage 元素，可能頁面未更新或網路異常"
                 send_line_message(msg)
                 logger.warning(msg)
 
